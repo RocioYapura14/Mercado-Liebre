c41beea5351dfdce5af2e45b0d1378d64a9da324
RocioYapura14 <yapura.rocio@outlook.com>
2022-06-28 00:24:16 -0300

    Servidor Corriendo

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+const port = 2020
+
+app.listen(port,() => console.log("Server runing in http://localhost:"+ port));
+app.use(express.static("public"));
+
+app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views","home.html")));
