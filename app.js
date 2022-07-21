e985892934f528f09a704ca619eb11308d623d02
RocioYapura14 <yapura.rocio@outlook.com>
2022-07-21 01:30:22 -0300

    Flexbox

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const app = express();
 const path = require ("path");
-const port = 3030
+const port = 2020
 
 app.listen(port,() => console.log("Server runing in http://localhost:"+ port));
 app.use(express.static("public"));
