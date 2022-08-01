0c2e1e05396e0f800438c6e1d88bda4e192ea484
RocioYapura14 <yapura.rocio@outlook.com>
2022-08-01 02:04:17 -0300

    formulario y login

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.listen(port,() => console.log("Server runing in http://localhost:"+ port));
 app.use(express.static("public"));
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views","home.html")));
+app.get("/register", (req,res) => res.sendFile(path.join(__dirname, "views","register.html")));
+app.get("/login", (req,res) => res.sendFile(path.join(__dirname, "views","login.html")));
